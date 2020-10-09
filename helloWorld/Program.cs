cbda7ef5b8781828834d999c1e56b404ccb031f0
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-10-09 10:55:38 -0300

    Declaração de Variáveis

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace helloWord.PasteTeste
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int variavelTeste;
+
+            variavelTeste = 123;
+
+            bool VariavelBoleana = false;
+
+            if(VariavelBoleana == false)
+                variavelTeste = 765;
+
+            var TesteInteger = 111;
+            var TesteBool =  true;
+            var TesteString = "Aula";    
+
+            Console.WriteLine("O Valor da Variavel é {0}", variavelTeste);
+            Console.WriteLine("O Valor da Variavel é {0}", TesteInteger);
+            Console.WriteLine("O Valor da Variavel é {0}", TesteBool);
+            Console.WriteLine("O Valor da Variavel é {0}", TesteString);
+            Console.ReadLine();
         }
     }
 }
